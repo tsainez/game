f7824428670825fe936e13f8cd5e06e033390407
tony <tsainez@gmail.com>
2020-07-29 19:08:11 -0700

    NewBehaviourScript

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // This is an example. 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
